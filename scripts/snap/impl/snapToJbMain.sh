34e8fe91bfd7f5f18b4c009f355dd0f3cc71783d
Igor Demin <igordmn@gmail.com>
2024-06-14 04:32:39 +0200

    Rename branches

--- scripts/snap/impl/snapToJbMain.sh
+++ scripts/snap/impl/snapToJbMain.sh
@@ -13,12 +13,12 @@ CURRENT_COMMIT=$(git rev-parse --short @)
 BRANCH_TO_RESTORE_IN_THE_END=$(git branch --show-current)
 
 
-TO_JB_MAIN_BRANCH=integration-snap/to-jb-main/$CURRENT_COMMIT
+TO_JB_MAIN_BRANCH=integration-snap/$CURRENT_COMMIT/to-jb-main
 git checkout --quiet $(git merge-base $CURRENT_COMMIT origin/jb-main) -B $TO_JB_MAIN_BRANCH
 $DIR/snapSubfolder.sh $CURRENT_COMMIT $ALL_FOLDERS
 echo "Created $TO_JB_MAIN_BRANCH"
 
-TO_INTEGRATION_BRANCH=integration-snap/to-integration/$CURRENT_COMMIT
+TO_INTEGRATION_BRANCH=integration-snap/$CURRENT_COMMIT/to-integration
 git checkout --quiet $(git merge-base $CURRENT_COMMIT origin/integration) -B $TO_INTEGRATION_BRANCH
 $DIR/mergeEmpty.sh $TO_JB_MAIN_BRANCH
 echo "Created $TO_INTEGRATION_BRANCH"
