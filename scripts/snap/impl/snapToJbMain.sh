c3fc8d6780fb7ae0976322fea1350c66a2349169
Igor Demin <igordmn@gmail.com>
2024-06-17 01:26:02 +0200

    Discussions

--- scripts/snap/impl/snapToJbMain.sh
+++ scripts/snap/impl/snapToJbMain.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+## See ../Readme.md for the purpose of this script
+
 set -e
 
 if [ -z "$1" ]; then
-echo "Specify the snapping subfolder. For example: ./snapToJbMain.sh androidx/compose-ui/1.6.0-alpha02 compose ':(exclude)compose/material3'"
+echo "Specify folders to snap to jb-main. For example: ./snapToJbMain.sh compose ':(exclude)compose/material3'"
 exit 1
 fi
 
@@ -14,12 +16,12 @@ BRANCH_TO_RESTORE_IN_THE_END=$(git branch --show-current)
 
 
 TO_JB_MAIN_BRANCH=integration-snap/$CURRENT_COMMIT/to-jb-main
-git checkout --quiet $(git merge-base $CURRENT_COMMIT origin/jb-main) -B $TO_JB_MAIN_BRANCH
+git checkout --quiet $(git merge-base $CURRENT_COMMIT jb-main) -B $TO_JB_MAIN_BRANCH
 $DIR/snapSubfolder.sh $CURRENT_COMMIT $ALL_FOLDERS
 echo "Created $TO_JB_MAIN_BRANCH"
 
 TO_INTEGRATION_BRANCH=integration-snap/$CURRENT_COMMIT/to-integration
-git checkout --quiet $(git merge-base $CURRENT_COMMIT origin/integration) -B $TO_INTEGRATION_BRANCH
+git checkout --quiet $(git merge-base $CURRENT_COMMIT integration) -B $TO_INTEGRATION_BRANCH
 $DIR/mergeEmpty.sh $TO_JB_MAIN_BRANCH
 echo "Created $TO_INTEGRATION_BRANCH"
 
